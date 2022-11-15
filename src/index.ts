df1f4b918867cded0502ac1f9bfacf353cbd29d4
Luis Coelho <mancoeluis@gmail.com>
2022-11-15 11:27:53 -0300

    chore: set up express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`)
+})
