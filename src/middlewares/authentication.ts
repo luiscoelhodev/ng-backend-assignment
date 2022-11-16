cdcd282bd275be1af8e22936a4dcba6b3ea077fa
Luis Coelho <mancoeluis@gmail.com>
2022-11-16 14:17:24 -0300

    fix: change decode to verify jwt method

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 import UsersService from "../providers/usersService"
 
 type JwtPayload = {
@@ -12,17 +13,24 @@ export const authMiddleware = async (request: Request, response: Response, next:
     const { authorization } = request.headers
 
     if (!authorization) {
-        return response.status(401).send({ message: 'No authorization was provided.' })
+        return response.status(401).send({ message: 'No authentication was provided.' })
     }
 
     const token = authorization.split(' ')[1]
-    const { id } = jwt.decode(token) as JwtPayload
+    if (!token) return response.status(400).json({ error: 'The token was not provided.' })
 
-    const userWasFound = await new UsersService().findById(id)
-    if (!userWasFound) return response.status(401).send({ message: 'Unauthorized.' })
+    try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET || '') as JwtPayload
+        
+        const userWasFound = await new UsersService().findById(id)
+        if (!userWasFound) return response.status(401).send({ message: 'Unauthorized.' })
 
-    const {password, ...rest} = userWasFound
+        const {password, ...rest} = userWasFound
 
-    request.user = rest
-    next()
+        request.user = rest
+        next()
+
+    } catch (error: any) {
+        return response.status(400).send({ error: error.message })
+    }
 }
